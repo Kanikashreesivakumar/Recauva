30a9d304ddbfb96228c197ae2b51327a3a4a5ccb
Kanikashreesivakumar <kanikashreesivakumar16@gmail.com>
2025-04-20 17:08:57 +0400

    fixed buttom

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -82,10 +82,10 @@ export default function Navigation() {
               </Link>
             ))}
             <Button 
-              className="bg-gradient-to-r from-reccova-green via-reccova-darkGreen to-reccova-lightGreen hover:from-reccova-darkGreen hover:to-reccova-green text-white font-semibold rounded-full px-6 transition-all duration-300 shadow-md hover:shadow-lg transform hover:-translate-y-0.5" 
+              className="bg-gradient-to-r  text-white font-semibold rounded-full px-6 transition-all duration-300 shadow-md hover:shadow-lg transform hover:-translate-y-0.5" 
               onClick={() => setIsBookingModalOpen(true)}
             >
-              <span className="text-white">Book Now</span>
+              <span className="text-black">Book Now</span>
             </Button>
           </nav>
 
@@ -142,7 +142,7 @@ export default function Navigation() {
                   </Link>
                 ))}
                 <Button
-                  className="btn-gradient text-white rounded-full w-full py-6"
+                  className="bg-gradient-to-r  text-white font-semibold rounded-full px-6 transition-all duration-300 shadow-md "
                   onClick={() => {
                     setIsMobileMenuOpen(false)
                     setIsBookingModalOpen(true)
